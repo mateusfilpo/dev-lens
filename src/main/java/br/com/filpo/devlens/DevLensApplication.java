307e3270501bff457a5a8b118ae3a6761748a1cd
Mateus Filpo <mateusfilpo27@gmail.com>
2026-03-29 15:44:33 -0300

    chore: scaffold inicial do projeto Dev-Lens

---
+++ src/main/java/br/com/filpo/devlens/DevLensApplication.java
@@ -0,0 +1,13 @@
+package br.com.filpo.devlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filpo/devlens/DevLensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filpo.devlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
